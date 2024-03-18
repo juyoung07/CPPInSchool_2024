73527a8865ee091e4c6b9d9a36b5d6196dd11278
juyoung07 <yunju5090@gmail.com>
2024-03-18 14:32:40 +0900

    변수의 중복을 막기 위한 namespace

--- 2024-CPP-201/02-01 기초.cpp
+++ 2024-CPP-201/02-01 기초.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+using namespace std;		// 더이상 std::를 쓰지 않아도 됨
 
 void main(void) {
 
-	// std : 이름공간 (namespace)
+	// std : 이름공간 (namespace), 변수의 중복을 막기 위한
 	// cout : 출력 객체
 	// endl: 줄바꿈(개행) 객체
-	std::cout << "Hello World" << std::endl;
+	cout << "Hello World" << endl;
 }
