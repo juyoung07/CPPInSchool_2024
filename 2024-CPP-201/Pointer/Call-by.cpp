4aeffb80c5c56434087b1f7e1297c4c5b3365948
juyoung07 <yunju5090@gmail.com>
2024-03-25 14:30:50 +0900

    Call by pointer

--- 2024-CPP-201/Pointer/Call-by.cpp
+++ 2024-CPP-201/Pointer/Call-by.cpp
@@ -1,19 +1,17 @@
 #include <iostream>
 using namespace std;
 
-void call_by_value(int a) {
-
-	// 새로운 지역변수 a를 만들고 값을 넣음
-
-	a = 10;
+void call_by_value(int *a) {
+	// a가 가리키고 있는 대상의 값을 바꿈
+	*a = 10;
 }
 
 int main(void) {
 	
 	int num = 3;
 	
-	// num의 값이 바뀌지 않음
-	call_by_value(num);
+	// num의 값이 바뀜
+	call_by_value(&num);
 	cout << num << endl;
 
 	return 0;
