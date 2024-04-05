9e3ef1c8cd725eca9d699c5b26d16ce751174efd
juyoung07 <yunju5090@gmail.com>
2024-04-05 09:53:19 +0900

    Juyoung 문자열 추가 - C 스타일

---
+++ 2024-CPP-201/02-04 문자열 추가.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+using namespace std;
+
+void main(void) {
+	char str[100] = "Juyoung";
+
+	// 문자열 추가
+	strcat(str, " is pretty 🥰");
+
+	printf("%s ", str);
+}
