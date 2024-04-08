b9805a1f0cd38a8d4cd3322c00472a3736657e14
juyoung07 <yunju5090@gmail.com>
2024-04-08 13:42:05 +0900

    Juyoung 문자열 비교 - C++ 스타일

---
+++ 2024-CPP-201/02-05 문자열 비교.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+void main(void) {
+	string str = "Juyoung";
+
+	// 문자열 비교
+	if (str == "Jiyoung")
+		cout << "같은 단어";
+	else
+		cout << "다른 단어";
+}
