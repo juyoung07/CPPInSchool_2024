bfa153ed0f1046b53b8f98fa8bde60146f328580
Yun juyoung <yunju5090@gmail.com>
2024-09-30 13:53:40 +0900

    09 - 02 범위기반 적용 전

---
+++ 2024-CPP-201/09-02 범위기반 for문.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(void) {
+    vector<int> arr;
+
+    arr.reserve(4);
+
+    arr.push_back(201);
+    arr.push_back(202);
+    arr.push_back(203);
+    arr.push_back(204);
+
+    for (int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << endl;
+    }
+
+}
