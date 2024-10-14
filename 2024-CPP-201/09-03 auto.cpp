b54da2e9cda1b422accb0421ae6821b632923a18
Yun juyoung <yunju5090@gmail.com>
2024-10-14 13:42:42 +0900

    09-03 auto 적용 전

---
+++ 2024-CPP-201/09-03 auto.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(void) {
+
+    int a = 40;
+    cout << a << endl;
+
+    vector<string> v = { "윤주영", "윤주일", "윤주이"};
+    cout << v[1] << endl;
+}
