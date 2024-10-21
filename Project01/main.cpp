84e76ee7cd2106350c81128d801ce198faa639a7
Yun juyoung <yunju5090@gmail.com>
2024-10-21 13:49:26 +0900

    [pj] 플레이어 이동

--- Project01/main.cpp
+++ Project01/main.cpp
@@ -17,12 +17,23 @@ void main(void) {
 		while (window.pollEvent(event)) {
 			if (event.type == Event::Closed)
 				window.close();
+
 		}
 
 		RectangleShape player;
 		player.setSize(Vector2f(200.f, 200.f));
 		player.setPosition((App::WIDTH - player.getSize().x)/2, (App::HEIGHT - player.getSize().y)/2);
 		player.setFillColor(Color::Magenta);
+
+		if (Keyboard::isKeyPressed(Keyboard::Key::Right))
+			player.move(10.f, 0.f);
+		if (Keyboard::isKeyPressed(Keyboard::Key::Left))
+			player.move(-10.f, 0.f);
+		if (Keyboard::isKeyPressed(Keyboard::Key::Up))
+			player.move(0.f, -10.f);
+		if (Keyboard::isKeyPressed(Keyboard::Key::Down))
+			player.move(0.f, 10.f);
+
 
 		window.clear(Color::White);
 		window.draw(player);
